7cdc67f6f65c753aa4b2e99304ff384a8641bc9b
hiroki <hiroki.kaminaga@cyber.ee>
2021-05-04 21:17:13 +0300

    section3-3&4: Method Notations.

---
+++ src/lectures/part2oop/MethodNotaions.scala
@@ -0,0 +1,66 @@
+package lectures.part2oop
+
+object MethodNotaions extends App {
+
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
+    def likes(movie: String): Boolean = movie == favoriteMovie
+    def +(person: Person): String =
+      s"${this.name} is hanging out with ${person.name}"
+    def +(title: String): String = s"$name ($title)"
+    def unary_+ : Person = new Person(name, favoriteMovie, age+1)
+    def unary_! : String = s"$name, what is the heck!?"
+    def isAlive: Boolean = true
+    def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+    def apply(n: Int): String = s"$name watched $favoriteMovie $n times."
+    def learns(sub: String): String = s"$name learns $sub"
+    def learnsScala: String = this.learns("Scala")
+  }
+
+  val mary = new Person("Mary", "Inception")
+  println(mary.likes("Inception"))
+  println(mary likes "Inception") // equivalent
+  // infix notation = operator notation (only when one arg) (syntactic sugar)
+
+  // "operations" in Scala
+  val tom = new Person("Tom", "Fight Club")
+  println(mary + tom)
+  println(mary.+(tom))
+
+  println(1 + 2)
+  println(1.+(2))
+  // ALL OPERATORS ARE METHODs.
+
+  // prefix notation
+  val x = -1
+  val y = 1.unary_-
+  // unary_ prefix only works with - + ~ !
+
+  println(!mary)
+  println(mary.unary_!)
+
+  // postfix notation
+  println(mary.isAlive)
+  println(mary isAlive) // available only without args
+
+  // "apply"
+  println(mary.apply())
+  println(mary()) // equivalent
+
+  /*
+    1. Overload the + operator
+      mary + "the rockstar" => new person "Mary (the rockstar)"
+    2. Add an age to the Person class
+      Add an unary + operator => new person with the age + 1
+      +mary => mary with the age incrementer
+    3. Add a "learns" method in the Person class => "Mary learns Scala"
+      Add a learnsScala method, calls learns method with "Scala"
+      Use it in postfix notation.
+    4. Overload the apply method
+      mary.apply(2) => "Mary watched Inception 2 times"
+   */
+
+  println(mary + "ths rockstar")
+  println((+mary).age)
+  println(mary learnsScala)
+  println(mary(2))
+}
